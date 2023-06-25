583f6d49b66f2ae2c2b2a0d78605c7bc9a89a554
Christopher A. Koster <christopher.koster@koster.group>
2023-06-25 11:15:31 -0500

    why is this tracking?

--- register.ps1
+++ register.ps1
@@ -1,6 +1,6 @@
 # Set the enivronment variables for the node script
-$env:DISCORD_APPLICATION_ID = 'APPLICATION_ID'
-$env:DISCORD_TOKEN = 'TOKEN'
+$env:DISCORD_APPLICATION_ID = 'testing'
+$env:DISCORD_TOKEN = 'testing'
 
 # Delete the dist folder if it exists
 if (Test-Path ./dist) {
